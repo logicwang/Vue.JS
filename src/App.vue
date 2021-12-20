aabccc653ea8d7c8acdf364b7b9425f2b2d401fb
热心市民王先生 <1758555877@qq.com>
2021-12-20 22:38:36 +0800

    /

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="show" type="button">xxxx</button>
+    <div v-if="seen" class="WYF">
+      <div class="box">
+        现在你看到我了
+        <button @click="show" type="button">关闭</button>
+      </div>
+    </div>
+    {{ message }}
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  methods: {
+    show(){
+      console.log('sss', this.seen)
+      this.seen = !this.seen
+    }
+  },
+  data() {
+    return {
+      message: 'XXX',
+      seen: false
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
@@ -21,8 +40,25 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+.WYF {
+  position:fixed;
+  background-color: #33333373;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+}
+.box {
+  background-color: #fff;
+  min-width: 320px;
+  min-height: 210px;
 }
 </style>
