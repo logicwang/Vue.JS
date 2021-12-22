53b6cde5dcb715d537b59e75b6eb2ff37e3f64cf
热心市民王先生 <1758555877@qq.com>
2021-12-22 11:18:50 +0800

    提交

---
+++ src/components/demo1.vue
@@ -0,0 +1,23 @@
+<template>
+   <div id="example">
+        <p>Original message: "{{ message }}"</p>
+        <p>Computed reversed message: "{{ reversedMessage }}"</p>
+    </div>
+</template>
+
+
+<script>
+        var vm = new Vue({
+            el: '#example',
+            data: {
+                message: 'WANGYUFEI'
+            },
+            computed: {
+                // 计算属性的 getter
+                reversedMessage: function () {
+                    // `this` 指向 vm 实例
+                    return this.message.split('').reverse().join('')
+                }
+            }
+        })
+</script>
