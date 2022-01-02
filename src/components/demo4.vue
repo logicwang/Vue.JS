e7f26b202ea2ec41565170fa4b26bbc9372598bb
热心市民王先生 <1758555877@qq.com>
2022-01-03 00:27:05 +0800

    列表渲染

--- src/components/demo4.vue
+++ src/components/demo4.vue
@@ -1,20 +1,41 @@
 <template>
-  <div id="app">
-    <ul id="example-1">
-      <li v-for="item in items" :key="item.message">
-        {{ item.message }}
+  <div>
+    <ul>
+      <li v-for="(item, index) in items" :key="index" v-on:click="sss(index)">
+        {{ item.aaa }} / {{index}}
       </li>
     </ul>
-  </div>
+
+    <ul>
+        {{OBJ.A}}
+    </ul>
+    <button v-on:click="onoooo">click </button>
+  </div> 
 </template>
 <script>
 export default {
-    e1: "#example-1",
-    data:()=> ({
-         items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ]
-    })
-}
+  data: () => ({
+    items: [
+      { message: "Foo", aaa: 22222 }, 
+      { message: "Bar", aaa: '' }
+      
+    ],
+    OBJ: {
+      A: 12222,
+    },
+  }),
+  methods: {
+    onoooo() {
+      this.OBJ.A = 44444
+      this.items.push({
+        message: "345",
+        aaa: 5555
+      })
+    },
+    sss(index) {
+      console.log(index)
+      this.items[index].aaa = 9999999
+    }
+  }
+};
 </script>
