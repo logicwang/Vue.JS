ef5a97cc1c819b24d6e9cab53f7e109dd7c4f31a
热心市民王先生 <1758555877@qq.com>
2022-01-02 19:43:25 +0800

    添加isgreen

--- src/components/demo3.vue
+++ src/components/demo3.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <div v-bind:class="{ active: isActive }"
+        <div v-bind:class="{ active: isActive , green : isGreen}"
             style="width: 200px;height: 200px;line-height: 200px;background-color: red;">
             hi,vue
         </div>
@@ -11,6 +11,11 @@ export default {
     e1: "#app",
     data:()=> ({
         isActive : true,
+        isGreen: true 
     })
 }
 </script>
+<style >
+.active{background: #FF0000;}
+.green{color: #00FF00;}
+</style>
