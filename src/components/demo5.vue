7f4070527104669125e264da766ba7ef47b7ba71
热心市民王先生 <1758555877@qq.com>
2022-01-03 14:21:14 +0800

    tijiao

---
+++ src/components/demo5.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul id="v-for-object" class="demo">
+      <li v-for="value in object" v-bind:key="value.id">
+        {{ value }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+new Vue({
+  el: "#v-for-object",
+  data: {
+    object: {
+      title: "How to do lists in Vue",
+      author: "Jane Doe",
+      publishedAt: "2016-04-10",
+    },
+  },
+});
+</script>
