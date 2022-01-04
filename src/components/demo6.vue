f6095dd9a501e4c4cc847d315a3dbb971198466a
热心市民王先生 <1758555877@qq.com>
2022-01-04 22:37:53 +0800

    用v-for在表单 <input>创建双向数据绑定

---
+++ src/components/demo6.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="e-1">
+    <input v-model="message" placeholder="edit me" />
+    <p>Message is: {{ message }}</p>
+  </div>
+</template>
+<script>
+export default {
+    e1: "#app",
+    data:()=> ({
+        message: "aaaa"
+
+    })
+}
+</script>
