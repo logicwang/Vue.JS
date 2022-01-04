285d252de890d6d4ca884a73f9ae97d84795b41c
热心市民王先生 <1758555877@qq.com>
2022-01-04 22:46:18 +0800

    添加多行文本

--- src/components/demo6.vue
+++ src/components/demo6.vue
@@ -2,14 +2,21 @@
   <div id="e-1">
     <input v-model="message" placeholder="edit me" />
     <p>Message is: {{ message }}</p>
+
+
+
+    <span>Multiline message2 is:</span>
+    <p style="white-space: pre-line">{{ message2 }}</p>
+    <br />
+    <textarea v-model="message2" placeholder="add multiple lines"></textarea>
   </div>
 </template>
 <script>
 export default {
-    e1: "#app",
-    data:()=> ({
-        message: "aaaa"
-
-    })
-}
+  e1: "#app",
+  data: () => ({
+    message: "aaaa",
+    message2: "bbb",
+  }),
+};
 </script>
