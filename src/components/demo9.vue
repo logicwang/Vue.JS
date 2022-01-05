e21f0c9a78bdaa254f9da30453dfb725e363301a
热心市民王先生 <1758555877@qq.com>
2022-01-05 17:11:24 +0800

    多选时 (绑定到一个数组)

---
+++ src/components/demo9.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <select v-model="selected" multiple style="width: 50px">
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <br />
+    <span>Selected: {{ selected }}</span>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    selected: [];
+  },
+};
+</script>
