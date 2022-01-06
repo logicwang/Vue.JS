2fc3a92d438c9b485f1c1121c6bb7d9ffd8e27d3
热心市民王先生 <1758555877@qq.com>
2022-01-06 21:57:21 +0800

    删除注释

--- src/components/demo11.vue
+++ src/components/demo11.vue
@@ -1,23 +1,12 @@
 <template>
-<button v-on:click="count++">You clicked me {{ count }} times.</button>
+  <button v-on:click="count++">You clicked me {{ count }} times.</button>
 </template>
 <script>
 export default {
   data() {
     return {
-
       count: 0,
-    }
+    };
   },
 };
-
-// Vue.component("button-counter", {
-//   data () {
-//     return {
-//       count: 0,
-//     };
-//   },
-//   template:
-//     '<button v-on:click="count++">You clicked me {{ count }} times.</button>',
-// });
 </script>
