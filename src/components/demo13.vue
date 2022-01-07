64fe3e264b86fefb49f7c6b15bb5ff9fda0b91f0
热心市民王先生 <1758555877@qq.com>
2022-01-07 16:39:17 +0800

    通过 Prop 向子组件传递数据

---
+++ src/components/demo13.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <blog-post title="My journey with Vue" :key="post"></blog-post>
+    <blog-post title="Blogging with Vue" :key="post"></blog-post>
+    <blog-post title="Why Vue is so fun" :key="post"></blog-post>
+    <blog-post
+      v-for="post in posts"
+      v-bind:key="post.id"
+      v-bind:title="post.title"
+    ></blog-post>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    posts: [
+      { id: 1, title: "My journey with Vue" },
+      { id: 2, title: "Blogging with Vue" },
+      { id: 3, title: "Why Vue is so fun" },
+    ];
+  },
+};
+</script>
