90ec467f7434d322efd079141339fbe3d863ec25
热心市民王先生 <1758555877@qq.com>
2022-01-08 14:19:40 +0800

    data 里有一个博文的数组

--- src/components/demo13.vue
+++ src/components/demo13.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
-    <blog-post title="My journey with Vue" :key="post"></blog-post>
+    <!-- <blog-post title="My journey with Vue" :key="post"></blog-post>
     <blog-post title="Blogging with Vue" :key="post"></blog-post>
-    <blog-post title="Why Vue is so fun" :key="post"></blog-post>
+    <blog-post title="Why Vue is so fun" :key="post"></blog-post> -->
     <blog-post
       v-for="post in posts"
       v-bind:key="post.id"
       v-bind:title="post.title"
-    ></blog-post>
+    >
+      <P>{{ post.title }}</P>
+    </blog-post>
   </div>
 </template>
 <script>
 export default {
   data() {
-    posts: [
-      { id: 1, title: "My journey with Vue" },
-      { id: 2, title: "Blogging with Vue" },
-      { id: 3, title: "Why Vue is so fun" },
-    ];
+    return {
+      posts: [
+        { id: 1, title: "My journey with Vue" },
+        { id: 2, title: "Blogging with Vue" },
+        { id: 3, title: "Why Vue is so fun" },
+      ],
+    };
   },
 };
 </script>
