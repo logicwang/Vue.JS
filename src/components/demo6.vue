50a1b47e20ba5819bb1b9a7d17fdb38cb1f50ede
热心市民王先生 <1758555877@qq.com>
2022-01-09 09:37:14 +0800

    添加p标签

--- src/components/demo6.vue
+++ src/components/demo6.vue
@@ -2,18 +2,26 @@
   <div id="e-1" style="margin-top: 20px">
     <input v-model="message" placeholder="edit me" />
     <p>Message is: {{ message }}</p>
-
     <span>Multiline message2 is:</span>
     <p style="white-space: pre-line">{{ message2 }}</p>
     <br />
     <ul>
-      <textarea v-model="message2" placeholder="add multiple lines"></textarea>
-      <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
-      <label for="jack">Jack</label>
-      <input type="checkbox" id="john" value="John" v-model="checkedNames" />
-      <label for="john">John</label>
-      <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
-      <label for="mike">Mike</label>
+      <p>
+        <textarea
+          v-model="message2"
+          placeholder="add multiple lines"
+        ></textarea>
+        <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+        <label for="jack">Jack</label>
+      </p>
+      <p>
+        <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+        <label for="john">John</label>
+      </p>
+      <p>
+        <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+        <label for="mike">Mike</label>
+      </p>
       <br />
     </ul>
     <span>Checked names: {{ checkedNames }}</span>
