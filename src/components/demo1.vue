a0789e7bb9b8aa4ccd8818970cfcb4d6cf849164
热心市民王先生 <1758555877@qq.com>
2022-01-14 12:30:55 +0800

    提交

--- src/components/demo1.vue
+++ src/components/demo1.vue
@@ -1,29 +1,27 @@
 <template>
-   <div class="example" id="example">
-        <p>Original message: "{{ message }}"</p>
-        <p>Computed reversed message: "{{ reversedMessage }}"</p>
-    </div>
+  <div >
+    <p>Original message: "{{ message }}"</p>
+    <p>Computed reversed message: "{{ reversedMessage }}"</p>
+  </div>
 </template>
 
-
 <script>
-var vm = new Vue({
-    el: '#example',
-    data: {
-        message: 'WANGYUFEI'
+export default {
+  data() {
+    return {
+      message: "wangyufeis",
+    };
+  },
+  computed: {
+    // 计算属性的 getter
+    reversedMessage: function () {
+      // `this` 指向 vm 实例
+      return this.message.split("").reverse().join("");
     },
-    computed: {
-        // 计算属性的 getter
-        reversedMessage: function () {
-            // `this` 指向 vm 实例
-            return this.message.split('').reverse().join('')
-        }
-    }
-})
+  },
+};
 </script>
 
 <style>
-.example {
-    background-color: red;
-}
+#app{margin-bottom: 60px;}
 </style>
