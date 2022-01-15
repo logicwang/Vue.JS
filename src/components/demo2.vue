39572addd1e7418fa4940dc6392cecb9744feba8
热心市民王先生 <1758555877@qq.com>
2022-01-15 13:51:12 +0800

    计算属性的版本

--- src/components/demo2.vue
+++ src/components/demo2.vue
@@ -3,17 +3,17 @@
 </template>
 
 <script>
-export default{
-  e1 : "demo",
-  return:{
-    data: {
-    firstName: 'Foo',
-    lastName: 'Bar'
+export default {
+  data() {
+    return {
+      firstName: "Foo",
+      lastName: "Bar",
+    };
   },
   computed: {
     fullName: function () {
-      return this.firstName + ' ' + this.lastName
-    }
-  }
-
+      return this.firstName + " " + this.lastName;
+    },
+  },
+};
 </script>
