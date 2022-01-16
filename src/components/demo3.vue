adb386dbbe92035b705020c2e642ece96b855cb4
热心市民王先生 <1758555877@qq.com>
2022-01-16 12:41:58 +0800

    demo3侦听器

--- src/components/demo3.vue
+++ src/components/demo3.vue
@@ -1,28 +1,54 @@
 <template>
-    <div id="app">
-        <div v-bind:class="{ active: isActive , green : isGreen}"
-            style="width: 200px;height: 200px;line-height: 200px;background-color: red;">
-            hi,vue
-        </div>
-        <div :style="{color: color,fontSize:size,background: background}">
-            Vue
-        </div>
-    </div>
+  <div >
+    <p>
+      Ask a yes/no question:
+      <input v-model="question" />
+    </p>
+    <p>{{ answer }}</p>
+  </div>
 </template>
-<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+<!-- 因为 AJAX 库和通用工具的生态已经相当丰富，Vue 核心代码没有重复 -->
+<!-- 提供这些功能以保持精简。这也可以让你自由选择自己更熟悉的工具。 -->
+<script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>
 <script>
 export default {
-    e1: "#app",
-    data:()=> ({
-        isActive : true,
-        isGreen : true ,
-        color:'red',
-        background :'black',
-        size: '50px',
-    })
-}
+  data: () => ({
+    question: "",
+    answer: "I cannot give you an answer until you ask a question!",
+  }),
+  watch: {
+    // 如果 `question` 发生改变，这个函数就会运行
+    question: function (newQuestion, oldQuestion) {
+      this.answer = "Waiting for you to stop typing...";
+      this.debouncedGetAnswer();
+    },
+  },
+  created() {
+    // `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
+    // 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
+    // AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
+    // `_.debounce` 函数 (及其近亲 `_.throttle`) 的知识，
+    // 请参考：https://lodash.com/docs#debounce
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500);
+  },
+  methods: {
+    getAnswer: function () {
+      if (this.question.indexOf("?") === -1) {
+        this.answer = "Questions usually contain a question mark. ;-)";
+        return;
+      }
+      this.answer = "Thinking...";
+      var vm = this;
+      axios
+        .get("https://yesno.wtf/api")
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer);
+        })
+        .catch(function (error) {
+          vm.answer = "Error! Could not reach the API. " + error;
+        });
+    },
+  },
+};
 </script>
-<style >
-.active{background-color: red;}
-.green{color: #00FF00;}
-</style>
