2eea819885e90729e17b2ac6644aa1e6daec5a4a
热心市民王先生 <1758555877@qq.com>
2022-01-17 10:24:05 +0800

    引入demo5

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
     <hr />
     <v-demo3></v-demo3>
     <hr />
-    <!-- <v-demo4></v-demo4> -->
+    <v-demo4></v-demo4>
     <!-- <hr /> -->
     <!-- <v-demo5></v-demo5> -->
     <!-- <v-demo6></v-demo6> -->
@@ -37,7 +37,7 @@
 import Demo1 from "./components/demo1.vue";
 import Demo2 from "./components/demo2.vue";
 import Demo3 from "./components/demo3.vue";
-// import Demo4 from "./components/demo4.vue";
+import Demo4 from "./components/demo4.vue";
 // import Demo5 from "./components/demo5.vue";
 // import Demo6 from "./components/demo6.vue";
 // import Demo7 from "./components/demo7.vue";
@@ -67,7 +67,7 @@ export default {
     "v-demo1": Demo1,
     "v-demo2": Demo2,
     "v-demo3": Demo3,
-    // "v-demo4": Demo4,
+    "v-demo4": Demo4,
     // "v-demo5": Demo5,
     // "v-demo6": Demo6,
     // "v-demo7": Demo7,
