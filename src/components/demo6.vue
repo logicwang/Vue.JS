5719522ec370f3a181702110f4659e906e1fdc2e
热心市民王先生 <1758555877@qq.com>
2022-01-17 20:09:34 +0800

    添加出处

--- src/components/demo6.vue
+++ src/components/demo6.vue
@@ -1,26 +1,33 @@
 <template>
   <div>
     <div
-      style="width: 200px; height: 200px; line-height: 200px;"
+      style="width: 200px; height: 200px; line-height: 200px"
       v-bind:class="{ active: isActive, green: isGreen }"
     >
       hi vue
     </div>
-    <div v-bind:style="{ color: activeColor, fontSize: fontSize + 'px' , background  : background}">
+    <div
+      v-bind:style="{
+        color: activeColor,
+        fontSize: fontSize + 'px',
+        background: background,
+      }"
+    >
       vue.js
     </div>
   </div>
+  <!-- 出处：https://cn.vuejs.org/v2/guide/class-and-style.html -->
 </template>
 <script>
-    export default {
-      data: () => ({
-        isActive: true,
-        isGreen: true,
-        activeColor: "red",
-        fontSize: 30,
-        background: "black"
-      }),
-    };
+      export default {
+        data: () => ({
+          isActive: true,
+          isGreen: true,
+          activeColor: "red",
+          fontSize: 30,
+          background: "black",
+        }),
+      };
 </script>
 <style>
 .active {
