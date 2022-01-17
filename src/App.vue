1b53ed8dfdcdea74748d01132b0df546a12ee246
热心市民王先生 <1758555877@qq.com>
2022-01-17 10:25:41 +0800

    引入新文件

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,8 @@
     <v-demo3></v-demo3>
     <hr />
     <v-demo4></v-demo4>
-    <!-- <hr /> -->
-    <!-- <v-demo5></v-demo5> -->
+    <hr />
+    <v-demo5></v-demo5>
     <!-- <v-demo6></v-demo6> -->
     <!-- <v-demo7></v-demo7> -->
     <!-- <v-demo8></v-demo8> -->
@@ -38,7 +38,7 @@ import Demo1 from "./components/demo1.vue";
 import Demo2 from "./components/demo2.vue";
 import Demo3 from "./components/demo3.vue";
 import Demo4 from "./components/demo4.vue";
-// import Demo5 from "./components/demo5.vue";
+import Demo5 from "./components/demo5.vue";
 // import Demo6 from "./components/demo6.vue";
 // import Demo7 from "./components/demo7.vue";
 // import Demo8 from "./components/demo8.vue";
@@ -68,7 +68,7 @@ export default {
     "v-demo2": Demo2,
     "v-demo3": Demo3,
     "v-demo4": Demo4,
-    // "v-demo5": Demo5,
+    "v-demo5": Demo5,
     // "v-demo6": Demo6,
     // "v-demo7": Demo7,
     // "v-demo8": Demo8,
