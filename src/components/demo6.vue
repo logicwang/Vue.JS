e1282c3a7ae283689e0b85246d148bc54771688e
热心市民王先生 <1758555877@qq.com>
2022-01-17 11:23:06 +0800

    对象语法

--- src/components/demo6.vue
+++ src/components/demo6.vue
@@ -1,39 +1,28 @@
 <template>
-  <div id="e-1" style="margin-top: 20px">
-    <input v-model="message" placeholder="edit me" />
-    <p>Message is: {{ message }}</p>
-    <span>Multiline message2 is:</span>
-    <p style="white-space: pre-line">{{ message2 }}</p>
-    <br />
-    <ul>
-      <p>
-        <textarea
-          v-model="message2"
-          placeholder="add multiple lines"
-        ></textarea>
-        <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
-        <label for="jack">Jack</label>
-      </p>
-      <p>
-        <input type="checkbox" id="john" value="John" v-model="checkedNames" />
-        <label for="john">John</label>
-      </p>
-      <p>
-        <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
-        <label for="mike">Mike</label>
-      </p>
-      <br />
-    </ul>
-    <span>Checked names: {{ checkedNames }}</span>
-  </div>
+    <div
+    class="text"
+      style="width: 200px; height: 200px; line-height: 200px"
+      v-bind:class="{ active: isActive, green: isGreen }"
+    >
+      hi vue
+    </div>
 </template>
 <script>
-export default {
-  e1: "#app",
-  data: () => ({
-    message: "aaaa",
-    message2: "bbb",
-    checkedNames: [],
-  }),
-};
+    export default {
+      data: () => ({
+        isActive: true,
+        isGreen: true,
+      }),
+    };
 </script>
+<style>
+      .active {
+        background: red;
+      }
+      .green {
+        color: yellow;
+      }
+      .text{
+        font-size: 40px;
+      }
+</style>
