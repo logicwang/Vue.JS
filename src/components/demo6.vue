d67df93dc7aa063c3e5e455de949c02bb66cc39e
热心市民王先生 <1758555877@qq.com>
2022-01-17 20:07:09 +0800

    绑定内联样式

--- src/components/demo6.vue
+++ src/components/demo6.vue
@@ -1,28 +1,33 @@
 <template>
+<div>
     <div
-    class="text"
-      style="width: 200px; height: 200px; line-height: 200px"
+      style="width: 200px; height: 200px; line-height: 200px "
       v-bind:class="{ active: isActive, green: isGreen }"
     >
       hi vue
     </div>
+    <div style="{color : color , fontSize : size}">
+      shit vue
+    </div>
+</div>
 </template>
 <script>
     export default {
       data: () => ({
         isActive: true,
         isGreen: true,
+        color: '#FF0000',
+        size: '60px'
       }),
     };
 </script>
 <style>
       .active {
         background: red;
+        margin-top: 50px;
+        font-size: 30px;
       }
       .green {
         color: yellow;
-      }
-      .text{
-        font-size: 40px;
       }
 </style>
