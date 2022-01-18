efa0779d2ef8199c231e04953ecf56a1880d4578
热心市民王先生 <1758555877@qq.com>
2022-01-18 13:35:49 +0800

    添加实例出处

--- src/components/demo7.vue
+++ src/components/demo7.vue
@@ -5,6 +5,7 @@
     <div v-else-if="type === 'c'">c</div>
     <div v-else>Not A/B/C</div>
     <h1 v-show="ko">h1111</h1>
+    <!-- 出处： https://cn.vuejs.org/v2/guide/conditional.html -->
   </div>
 </template>
 <script>
