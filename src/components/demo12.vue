7a410831a049fdc6a681f59a3e010486ed0f0fea
热心市民王先生 <1758555877@qq.com>
2022-01-19 00:00:26 +0800

    显示一个数组经过过滤或排序后的版本而不是实际变更或者重置原始数据，这样的话，就要创建一个计算属性，用于返回过滤或者排序后的数组

--- src/components/demo12.vue
+++ src/components/demo12.vue
@@ -1,16 +1,22 @@
 <template>
-<div>
-  <button v-on:click="count++">You clicked me {{ count }} times.</button>
-  <button v-on:click="count++">You clicked me {{ count }} times.</button>
-  <button v-on:click="count++">You clicked me {{ count }} times.</button>
-</div>
+<ul>
+  <li v-for="n in evenNumbers " :key="n">{{ n }}</li>
+</ul>
+<!-- 出处：https://cn.vuejs.org/v2/guide/list.html   显示过滤/排序后的结果 -->
 </template>
 <script>
 export default {
   data() {
     return {
-      count: 0,
+      numbers: [ 1, 2, 3, 4, 5 ]
     };
   },
+  computed: {
+  evenNumbers: function () {
+    return this.numbers.filter(function (number) {
+      return number % 2 === 0
+    })
+  }
+}
 };
 </script>
