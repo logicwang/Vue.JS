2b6c8afbbcab4434be2dbccf10c62ba874e41ba1
热心市民王先生 <1758555877@qq.com>
2022-01-21 18:12:06 +0800

    rm重复文件 以及取消注释app中的内容

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <!-- <hr />
+    <hr />
     <button @click="show" type="button">xxxx</button> -->
-    <!-- <div v-if="seen" class="WYF">
+    <div v-if="seen" class="WYF">
       <div class="box">
         现在你看到我了
         <button @click="show" type="button">关闭</button>
       </div>
-    </div> -->
-    <!-- {{ message }} -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <!-- <v-demo1></v-demo1>
+    </div>
+    {{ message }}
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-demo1></v-demo1>
     <hr />
     <v-demo2></v-demo2>
     <hr />
@@ -33,41 +33,41 @@
     <v-demo11></v-demo11>
     <hr />
     <v-demo12></v-demo12>
-    <hr /> -->
+    <hr />
     <v-demo13></v-demo13>
-    <!-- <hr />
+    <hr />
     <v-demo14></v-demo14>
     <hr />
     <v-demo15></v-demo15>
     <hr />
-    <v-demo16></v-demo16> 
+    <v-demo16></v-demo16>
     <hr />
     <v-demo17></v-demo17>
     <hr />
-    <v-demo18></v-demo18> -->
+    <v-demo18></v-demo18>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-// import Demo1 from "./components/demo1.vue";
-// import Demo2 from "./components/demo2.vue";
-// import Demo3 from "./components/demo3.vue";
-// import Demo4 from "./components/demo4.vue";
-// import Demo5 from "./components/demo5.vue";
-// import Demo6 from "./components/demo6.vue";
-// import Demo7 from "./components/demo7.vue";
-// import Demo8 from "./components/demo8.vue";
-// import Demo9 from "./components/demo9.vue";
-// import Demo10 from "./components/demo10.vue";
-// import Demo11 from "./components/demo11.vue";
-// import Demo12 from "./components/demo12.vue";
+import Demo1 from "./components/demo1.vue";
+import Demo2 from "./components/demo2.vue";
+import Demo3 from "./components/demo3.vue";
+import Demo4 from "./components/demo4.vue";
+import Demo5 from "./components/demo5.vue";
+import Demo6 from "./components/demo6.vue";
+import Demo7 from "./components/demo7.vue";
+import Demo8 from "./components/demo8.vue";
+import Demo9 from "./components/demo9.vue";
+import Demo10 from "./components/demo10.vue";
+import Demo11 from "./components/demo11.vue";
+import Demo12 from "./components/demo12.vue";
 import Demo13 from "./components/demo13.vue";
-// import Demo14 from "./components/demo14.vue";
-// import Demo15 from "./components/demo15.vue";
-// import Demo16 from "./components/demo16.vue";
-// import Demo17 from "./components/demo17.vue";
-// import Demo18 from "./components/demo18.vue";
+import Demo14 from "./components/demo14.vue";
+import Demo15 from "./components/demo15.vue";
+import Demo16 from "./components/demo16.vue";
+import Demo17 from "./components/demo17.vue";
+import Demo18 from "./components/demo18.vue";
 export default {
   name: "App",
   methods: {
@@ -83,24 +83,24 @@ export default {
     };
   },
   components: {
-    // "v-demo1": Demo1,
-    // "v-demo2": Demo2,
-    // "v-demo3": Demo3,
-    // "v-demo4": Demo4,
-    // "v-demo5": Demo5,
-    // "v-demo6": Demo6,
-    // "v-demo7": Demo7,
-    // "v-demo8": Demo8,
-    // "v-demo9": Demo9,
-    // "v-demo10": Demo10,
-    // "v-demo11": Demo11,
-    // "v-demo12": Demo12,
+    "v-demo1": Demo1,
+    "v-demo2": Demo2,
+    "v-demo3": Demo3,
+    "v-demo4": Demo4,
+    "v-demo5": Demo5,
+    "v-demo6": Demo6,
+    "v-demo7": Demo7,
+    "v-demo8": Demo8,
+    "v-demo9": Demo9,
+    "v-demo10": Demo10,
+    "v-demo11": Demo11,
+    "v-demo12": Demo12,
     "v-demo13": Demo13,
-    // "v-demo14": Demo14,
-    // "v-demo15": Demo15,
-    // "v-demo16": Demo16,
-    // "v-demo17": Demo17,
-    // "v-demo18": Demo18,
+    "v-demo14": Demo14,
+    "v-demo15": Demo15,
+    "v-demo16": Demo16,
+    "v-demo17": Demo17,
+    "v-demo18": Demo18,
     // HelloWorld
   },
 };
