fde03bc8b48b421dfdd0a1e99c326000d844cc84
热心市民王先生 <1758555877@qq.com>
2022-01-22 15:18:38 +0800

    v-for可以访问父作用域的property同时也能支持一个可以选择的第二个参数

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ parentMessage }} - {{ index }} - {{ item.message }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
+// export default {
+//   name: "App",
+//   components: {
+//     HelloWorld,
+//   },
+// };
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      parentMessage: "Parent",
+      items: [
+      { message: "Foo" },
+      { message: "Bar" }
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+/* #app  */
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
