36b39113e74fbd4d81a2bc7d4fed14681c17da74
logicwang <1758555877@qq.com>
2022-01-24 01:25:29 +0800

    在组件中使用v-for

---
+++ demo7/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <hello-world msg="121212"></hello-world>
+    <blog-post
+  v-for="post in posts"
+  v-bind:key="post.id"
+  v-bind:title="post.title"
+></blog-post>
+
+    <blog-post title="My journey with Vue"></blog-post>
+    <blog-post title="Blogging with Vue"></blog-post>
+    <blog-post title="Why Vue is so fun"></blog-post>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import BlogPost from './components/HelloWorld1.vue';
+export default{
+  name:'APP',
+  components:{
+    HelloWorld,
+    BlogPost
+  },
+  data() {
+    return{
+      posts: [
+      { id: 1, title: 'My journey with Vue' },
+      { id: 2, title: 'Blogging with Vue' },
+      { id: 3, title: 'Why Vue is so fun' }
+    ]
+    }
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
