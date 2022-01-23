a7b982f73870ec2718299254cef84ffb4f1908c7
热心市民王先生 <1758555877@qq.com>
2022-01-23 22:03:39 +0800

    vue组件实例

--- demo5/src/App.vue
+++ demo5/src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <button-counter></button-counter>
+    <button v-on:click="count++">你点击了我 {{ count }} 下</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  // name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
