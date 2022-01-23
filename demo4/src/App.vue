c8c5f6686ce5da6cddf18933317601dcd614f63d
热心市民王先生 <1758555877@qq.com>
2022-01-23 19:34:33 +0800

    v-for能接受整数但是在这种情况之下它会把模板重复对应次数

--- demo4/src/App.vue
+++ demo4/src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+      <span v-for="n in 10" :key="n">{{ n }} </span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+export default{
+  data(){
+    return{
+       sets: 
+       [[ 1, 2, 3, 4, 5 ], 
+       [6, 7, 8, 9, 10]]
+    }
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
