4a53eb8ebcac6f538eae85c106c513896cee7fbc
logicwang <1758555877@qq.com>
2022-01-24 11:59:56 +0800

    组件

--- demo7/src/App.vue
+++ demo7/src/App.vue
@@ -9,17 +9,21 @@
     <blog-post title="My journey with Vue"></blog-post>
     <blog-post title="Blogging with Vue"></blog-post>
     <blog-post title="Why Vue is so fun"></blog-post>
+
+    <v-demo1></v-demo1>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import BlogPost from "./components/HelloWorld1.vue";
+import Demo1 from "./components/demo1.vue";
 export default {
   name: "APP",
   components: {
     HelloWorld,
     BlogPost,
+    "v-demo1": Demo1,
   },
   data() {
     return {
@@ -29,6 +33,11 @@ export default {
         { id: 3, title: "Why Vue is so fun" },
       ],
     };
+  },
+  methods: {
+    onEnlargeText: function (enlargeAmount) {
+      this.postFontSize += enlargeAmount;
+    },
   },
 };
 </script>
--- demo7/src/components/HelloWorld1.vue
+++ demo7/src/components/HelloWorld1.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h3>{{ title }}</h3>
+    
   </div>
+  
 </template>
 <script>
 
