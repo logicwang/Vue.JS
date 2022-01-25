59736a8beb9fd8191269bef8c38a72faf8234759
logicwang <1758555877@qq.com>
2022-01-25 17:12:54 +0800

    v-on 指令监听 DOM 事件

--- demo9/src/App.vue
+++ demo9/src/App.vue
@@ -1,19 +1,26 @@
 <template>
-  <div >
+  <div>
+    <button v-on:click="counter += 1">Add 1</button>
+    <p>The button above has been clicked {{ counter }} times.</p>
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   // name: 'App',
   // components: {
   //   HelloWorld
   // }
-}
+  data() {
+    return{
+      counter: 0
+    }
+  }
+};
 </script>
 
 <style>
