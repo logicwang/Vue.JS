f45c7bc100acfce06fb62ddee678e03077593b59
logicwang <1758555877@qq.com>
2022-01-25 18:06:34 +0800

    引入demo2

--- demo9/src/App.vue
+++ demo9/src/App.vue
@@ -7,12 +7,15 @@
     <hr />
     <v-demo1></v-demo1>
     <hr />
+    <v-demo2></v-demo2>
+    <hr />
   </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
 import Demo1 from "./components/demo1.vue";
+import Demo2 from "./components/demo2.vue";
 
 export default {
   // name: 'App',
@@ -26,6 +29,7 @@ export default {
   },
   components:{
     "v-demo1": Demo1,
+    "v-demo2": Demo2,
   }
 };
 </script>
