04f0eff3ec728c93d7017e6bd8e909503cfbfde8
logicwang <1758555877@qq.com>
2022-01-25 18:08:18 +0800

    除了直接调用方法，可以内联js语句中调用方法

---
+++ demo9/src/components/demo2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button v-on:click="say('hi')">Say hi</button>
+    <button v-on:click="say('what')">Say what</button>
+  </div>
+</template>
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+  methods: {
+    say: function (message) {
+      alert(message);
+    },
+  },
+};
+</script>
