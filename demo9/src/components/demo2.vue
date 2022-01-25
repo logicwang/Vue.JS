7ea23e62b70cebb25f6156e7e77ab619549dacf2
logicwang <1758555877@qq.com>
2022-01-25 18:10:54 +0800

    用特殊变量 把它传入方法中

--- demo9/src/components/demo2.vue
+++ demo9/src/components/demo2.vue
@@ -2,6 +2,9 @@
   <div>
     <button v-on:click="say('hi')">Say hi</button>
     <button v-on:click="say('what')">Say what</button>
+    <button v-on:click="warn('Form cannot be submitted yet.', $event)">
+      Submit
+    </button>
   </div>
 </template>
 <script>
@@ -10,6 +13,13 @@
 export default {
   methods: {
     say: function (message) {
+      alert(message);
+    },
+    warn: function (message, event) {
+      // 现在我们可以访问原生事件对象
+      if (event) {
+        event.preventDefault();
+      }
       alert(message);
     },
   },
