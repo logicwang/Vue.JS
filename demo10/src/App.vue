c7f91b880c29ba8a058831f5eccc236fd50eea7f
logicwang <1758555877@qq.com>
2022-01-26 01:57:54 +0800

    v-for 渲染的动态选项

--- demo10/src/App.vue
+++ demo10/src/App.vue
@@ -15,6 +15,8 @@
     <hr />
     <v-demo7></v-demo7>
     <hr />
+    <v-demo8></v-demo8>
+    <hr />
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     
@@ -30,6 +32,7 @@ import Demo4 from "./components/demo4.vue";
 import Demo5 from "./components/demo5.vue";
 import Demo6 from "./components/demo6.vue";
 import Demo7 from "./components/demo7.vue";
+import Demo8 from "./components/demo8.vue";
 
 export default {
   components: {
@@ -40,6 +43,7 @@ export default {
     "v-demo5": Demo5,
     "v-demo6": Demo6,
     "v-demo7": Demo7,
+    "v-demo8": Demo8,
   },
   // name: 'App',
   // components: {
