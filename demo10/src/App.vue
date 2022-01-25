4d4961abc38a045ae749c04dd5878b623a5379f2
logicwang <1758555877@qq.com>
2022-01-25 22:19:25 +0800

    多个复选框绑定到同一个数组

--- demo10/src/App.vue
+++ demo10/src/App.vue
@@ -7,6 +7,9 @@
     <hr />
     <v-demo2></v-demo2>
     <hr />
+    <v-demo3></v-demo3>
+    <hr />
+    <!--出处： https://cn.vuejs.org/v2/guide/forms.html -->
   </div>
 </template>
 
@@ -14,11 +17,13 @@
 // import HelloWorld from "./components/HelloWorld.vue";
 import Demo1 from "./components/demo1.vue";
 import Demo2 from "./components/demo2.vue";
+import Demo3 from "./components/demo3.vue";
 
 export default {
   components: {
     "v-demo1": Demo1,  
     "v-demo2": Demo2,
+    "v-demo3": Demo3,
   },
   // name: 'App',
   // components: {
@@ -35,6 +40,7 @@ export default {
 <style>
 hr {
   margin-top: 20px;
+  margin-bottom: 20px;
   border: 3px solid #ff000f;
 }
 div {
