ca3ef4dd9df6911d7f10ad329fb4efa5869b4e13
logicwang <1758555877@qq.com>
2022-01-25 17:54:18 +0800

    不能把js代码写在v-on指令中。需要一个调用的方法名称

---
+++ demo9/src/components/demo1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- `greet` 是在下面定义的方法名 -->
+    <button v-on:click="greet">Greet</button>
+  </div>
+</template>
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+  data() {
+    return {
+      name: "Vue.js",
+    };
+  },
+  // 在 `methods` 对象中定义方法
+  methods: {
+    greet: function (event) {
+      // `this` 在方法里指向当前 Vue 实例
+      alert("Hello " + this.name + "!");
+      // `event` 是原生 DOM 事件
+      if (event) {
+        alert(event.target.tagName);
+      }
+    },
+  },
+};
+</script>
