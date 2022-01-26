bc787a3472653edd9ee00d9edf7366da9bf62287
logicwang <1758555877@qq.com>
2022-01-27 01:14:42 +0800

    引入新实例

--- demo11/src/App.vue
+++ demo11/src/App.vue
@@ -7,6 +7,7 @@
     <hr />
     <v-demo3></v-demo3>
     <hr />
+    <v-demo4></v-demo4>
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <!--出处： https://cn.vuejs.org/v2/guide/components.html#data-%E5%BF%85%E9%A1%BB%E6%98%AF%E4%B8%80%E4%B8%AA%E5%87%BD%E6%95%B0 -->
@@ -17,11 +18,13 @@
 import Demo1 from "./components/demo1.vue";
 import Demo2 from "./components/demo2.vue";
 import Demo3 from "./components/demo3.vue";
+import Demo4 from "./components/demo4.vue";
 export default {
   components: {
     "v-demo1": Demo1,
     "v-demo2": Demo2,
     "v-demo3": Demo3,
+    "v-demo4": Demo4,
   },
 };
 </script>
