13fadb743d601b5d68fe706692f6b5f41ab42542
logicwang <1758555877@qq.com>
2022-01-27 01:07:16 +0800

    一个组件的 data 选项必须是一个函数

--- demo11/src/App.vue
+++ demo11/src/App.vue
@@ -5,18 +5,23 @@
     <hr />
     <v-demo2></v-demo2>
     <hr />
+    <v-demo3></v-demo3>
+    <hr />
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!--出处： https://cn.vuejs.org/v2/guide/components.html#data-%E5%BF%85%E9%A1%BB%E6%98%AF%E4%B8%80%E4%B8%AA%E5%87%BD%E6%95%B0 -->
   </div>
 </template>
 
 <script>
 import Demo1 from "./components/demo1.vue";
 import Demo2 from "./components/demo2.vue";
+import Demo3 from "./components/demo3.vue";
 export default {
   components: {
     "v-demo1": Demo1,
     "v-demo2": Demo2,
+    "v-demo3": Demo3,
   },
 };
 </script>
