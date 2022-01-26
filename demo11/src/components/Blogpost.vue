5e06df77002a8dd88b3b7d9942cc3bcd3ec44913
logicwang <1758555877@qq.com>
2022-01-27 01:14:24 +0800

    通过 Prop 向子组件传递数据

---
+++ demo11/src/components/Blogpost.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <blog-post
+      v-for="post in posts"
+      v-bind:key="post.id"
+      v-bind:title="post.title"
+    ></blog-post>
+  </div>
+</template>
+<script>
+    export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "My journey with Vue" },
+        { id: 2, title: "Blogging with Vue" },
+        { id: 3, title: "Why Vue is so fun" },
+      ],
+    };
+  },
+
+};
+</script>
---
+++ demo11/src/components/demo4.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <blog-post title="My journey with Vue"></blog-post>
+    <blog-post title="Blogging with Vue"></blog-post>
+    <blog-post title="Why Vue is so fun"></blog-post>
+    <Blogpost></Blogpost>
+  </div>
+</template>
+<script>
+import Blogpost from "./Blogpost.vue";
+export default {
+  components: {
+    Blogpost,
+  },
+};
+</script>
