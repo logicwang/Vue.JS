c87c43334fd529cdfcd7001bb6313b1c5f0d6efd
logicwang <1758555877@qq.com>
2022-01-28 14:55:23 +0800

    添加出bug之后和修改之后的区别

--- demo11/src/components/HelloWorld1.vue
+++ demo11/src/components/HelloWorld1.vue
@@ -1,23 +1,30 @@
 <template>
   <div>
-    <blog-post
+    <h3>{{ title }}</h3>
+    <!-- <blog-post
       v-for="post in posts"
       v-bind:key="post.id"
       v-bind:title="post.title"
-    ></blog-post>
+    ></blog-post> -->
   </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      posts: [
-        { id: 1, title: "My journey with Vue" },
-        { id: 2, title: "Blogging with Vue" },
-        { id: 3, title: "Why Vue is so fun" },
-      ],
-    };
+  name: "BlogPost",
+  props: {
+    title: String,
   },
-
 };
+// export default {
+//   data() {
+//     return {
+//       posts: [
+//         { id: 1, title: "My journey with Vue" },
+//         { id: 2, title: "Blogging with Vue" },
+//         { id: 3, title: "Why Vue is so fun" },
+//       ],
+//     };
+//   },
+
+// };
 </script>
