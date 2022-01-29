5cbc714659b8144b401ded238c6e3026662c798d
logicwang <1758555877@qq.com>
2022-01-29 11:24:04 +0800

    提交

--- demo11/src/components/demo4.vue
+++ demo11/src/components/demo4.vue
@@ -3,15 +3,18 @@
     <blog-post title="My journey with Vue"></blog-post>
     <blog-post title="Blogging with Vue"></blog-post>
     <blog-post title="Why Vue is so fun"></blog-post>
-    <BlogPost></Blogpost>
+    <!-- <BlogPost></Blogpost> -->
+    <WorldHellow></WorldHellow>
   </div>
 </template>
 <script>
 import BlogPost from './HelloWorld1.vue'
+import WorldHellow from './HelloWorld2.vue'
 export default {
   name: "App",
   components: {
    BlogPost,
+   WorldHellow
   },
 };
 </script>
