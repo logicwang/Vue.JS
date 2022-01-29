8e28a5cb841622aa7419d1dc3a966e8720ff1733
logicwang <1758555877@qq.com>
2022-01-29 14:06:07 +0800

    demo12

---
+++ demo12/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <hr />
+    <v-demo1></v-demo1>
+    <hr />
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+import Demo1 from "./components/demo1.vue";
+export default {
+  components: {
+    "v-demo1": Demo1,
+  },
+};
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
+
+<style>
+hr {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: 3px solid #ff000f;
+}
+div {
+  text-align: center;
+}
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
