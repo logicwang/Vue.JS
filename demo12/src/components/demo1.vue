38e85879ea7835fb8a5161c5ef8e2d184382a277
logicwang <1758555877@qq.com>
2022-01-29 14:35:57 +0800

    一个简单的vue组件

--- demo12/src/components/demo1.vue
+++ demo12/src/components/demo1.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-    </div>
+  <div>
+    <h2>ssss</h2>
+    <button @click="run()">执行run方法</button>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-
-    }
-}
+  data() {
+    return {
+      msg: "我是组建首页!",
+    };
+  },
+  methods:{
+      run(){
+          alert('hello!');
+      }
+  }
+};
 </script>
