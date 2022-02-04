a99c6a986fee19ba876aa8a3896fc6e11b65df6c
logicwang <1758555877@qq.com>
2022-02-04 19:55:58 +0800

    ad

---
+++ demo12/src/components/demo3.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <blog-post title="My journey with Vue"></blog-post>
+    <blog-post title="Blogging with Vue"></blog-post>
+    <blog-post title="Why Vue is so fun"></blog-post>
+    <!-- <BlogPost></Blogpost> -->
+    <!-- <WorldHellow></WorldHellow> -->
+  </div>
+</template>
+<script>
+import BlogPost from './HelloWorld1.vue'
+// import WorldHellow from './HelloWorld2.vue'
+export default {
+  name: "App",
+  components: {
+   BlogPost,
+  //  WorldHellow
+  },
+};
+</script>
