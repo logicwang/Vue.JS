7a45205ccb2509d02bc5714bcd3073aa6ca3cf2a
logicwang <1758555877@qq.com>
2022-02-07 10:48:36 +0800

    监听子组件事件

--- demo12/src/components/HelloWorld1.vue
+++ demo12/src/components/HelloWorld1.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
     <h3>{{ title }}</h3>
+    <button v-on:click="$emit('enlarge-text')">Enlarge text</button>
+    <!-- <blog-post ... v-on:enlarge-text="postFontSize += 1"></blog-post> -->
+    
   </div>
 </template>
 <script>
-
 export default {
-  name:"BlogPost",
-    props: {
-    title: String
+  name: "BlogPost",
+  props: {
+    title: String,
   },
+  // data(){
+  //   return{
+  //      posts: [
+  //     { id: 1, title: 'My journey with Vue' },
+  //     { id: 2, title: 'Blogging with Vue' },
+  //     { id: 3, title: 'Why Vue is so fun' }
+  //   ]
+  //   }
+  // }
 };
 </script>
