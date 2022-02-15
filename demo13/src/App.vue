c3ca4cc0d517b0b4b33359cdee592b5df0cd13b3
logicwang <1758555877@qq.com>
2022-02-15 20:06:45 +0800

    删除注释内容

--- demo13/src/App.vue
+++ demo13/src/App.vue
@@ -3,19 +3,14 @@
     <hr />
     <v-demo1></v-demo1>
     <hr />
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 import Demo1 from "./components/demo1.vue";
-// import HelloWorld from './components/HelloWorld.vue'
 export default {
-  // name: 'App',
   components: {
     "v-demo1": Demo1,
-    // HelloWorld
   }
 }
 </script>
@@ -29,12 +24,4 @@ hr {
 div {
   text-align: center;
 }
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
 </style>
