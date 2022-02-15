86e375c568df6947d01f0d7a3eeb7c8e0b9d5587
logicwang <1758555877@qq.com>
2022-02-15 13:08:31 +0800

    prop用法

---
+++ demo13/src/components/sonComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>子
+    {{ fatherMsg }}
+    <button @click="change">按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sonComponent',
+  props: ['fatherMsg'],
+  methods: {
+    change() {
+      this.fatherMsg = '666666666'
+    }
+  }
+}
+</script>
