6be41c8481fb8b23bd72e557e8041cad162d2bd4
logicwang <1758555877@qq.com>
2022-02-17 17:18:19 +0800

    Vue中props传递参数

---
+++ demo13/src/components/WelloHorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Linkssss</h2>
+    <h3>{{ message1 }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelloHorld',
+  props: ['message1'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
