4e8cf59e8f9d54a076c7d62674e8dd37d6752c7a
logicwang <1758555877@qq.com>
2022-02-19 15:59:19 +0800

    传递动态props

--- demo13/src/App.vue
+++ demo13/src/App.vue
@@ -11,6 +11,7 @@
     <hr />
     <v-demo5></v-demo5>
     <hr />
+    <v-demo6></v-demo6>
   </div>
 </template>
 
@@ -20,6 +21,7 @@ import Demo2 from "./components/demo2.vue";
 import Demo3 from "./components/demo3.vue";
 import Demo4 from "./components/demo4.vue";
 import Demo5 from "./components/demo5.vue";
+import Demo6 from "./components/demo6.vue";
 export default {
   components: {
     "v-demo1": Demo1,
@@ -27,6 +29,7 @@ export default {
     "v-demo3": Demo3,
     "v-demo4": Demo4,
     "v-demo5": Demo5,
+    "v-demo6": Demo6,
   }
 }
 </script>
---
+++ demo13/src/components/demo6.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      父级:
+      <input type="text" v-model="message">
+    </div>
+    <v-child info="message"></v-child>
+  </div>
+</template>
+<script>
+import Chld from './Chld.vue'
+export default {
+  components:{
+    'v-child':Chld
+  },
+  data(){
+    return{
+      message:'asdasd'
+    }
+  }
+}
+</script>
