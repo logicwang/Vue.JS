d8e1a6ab9439728ce3a535dce3ff7e641328f307
logicwang <1758555877@qq.com>
2022-02-20 10:32:29 +0800

    vue生命周期

--- demo13/src/App.vue
+++ demo13/src/App.vue
@@ -12,6 +12,8 @@
     <v-demo5></v-demo5>
     <hr />
     <v-demo6></v-demo6>
+    <hr />
+    <v-demo7></v-demo7>
   </div>
 </template>
 
@@ -22,6 +24,7 @@ import Demo3 from "./components/demo3.vue";
 import Demo4 from "./components/demo4.vue";
 import Demo5 from "./components/demo5.vue";
 import Demo6 from "./components/demo6.vue";
+import Demo7 from "./components/demo7.vue";
 export default {
   components: {
     "v-demo1": Demo1,
@@ -30,6 +33,7 @@ export default {
     "v-demo4": Demo4,
     "v-demo5": Demo5,
     "v-demo6": Demo6,
+    "v-demo7": Demo7,
   }
 }
 </script>
