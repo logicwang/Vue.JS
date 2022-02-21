66fde0ccd01dcd40fa7efbfcd7f644671538ea27
logicwang <1758555877@qq.com>
2022-02-21 11:06:40 +0800

    接收过来的数据储存在data中的一个临时值

---
+++ demo13/src/components/demo8.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ cmsg }}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      cmsg: this.msg,
+    };
+  },
+  props: ["msg"],
+  created() {
+    this.cmsg = 666;
+  },
+};
+</script>
