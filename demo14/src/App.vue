69d058ccec91243a5a89410b5b301d0cd93d6eba
logicwang <1758555877@qq.com>
2022-02-21 11:40:45 +0800

    引入demo1

--- demo14/src/App.vue
+++ demo14/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <hr />
+    <v-demo1></v-demo1>
+    <hr />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Demo1 from "./components/demo1.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    "v-demo1": Demo1,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+hr {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: 3px solid #ff000f;
+}
+div {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
