a125c0ba505c5fe75771378ff3704bcf534f4f07
logicwang <1758555877@qq.com>
2022-02-21 11:40:29 +0800

    vue自定义组件实现v-model双向绑定

---
+++ demo14/src/components/child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>我是子组件</p>
+    <p>子组件内容:{{ value }}</p>
+    <p><button @click="onChange">改变内容</button></p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    }
+  },
+  methods: {
+    onChange() {
+      this.value = '我是由子组件触发改变了内容'
+    }
+  }
+}
+</script>
