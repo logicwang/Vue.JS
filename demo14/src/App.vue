618de3fbeb6d6a54813874f8b1aafa92cc637251
logicwang <1758555877@qq.com>
2022-02-23 18:49:02 +0800

    引入demo4

--- demo14/src/App.vue
+++ demo14/src/App.vue
@@ -6,6 +6,8 @@
     <v-demo2></v-demo2>
     <hr />
     <v-demo3></v-demo3>
+    <hr />
+    <v-demo3></v-demo3>
   </div>
 </template>
 
@@ -13,12 +15,14 @@
 import Demo1 from "./components/demo1.vue";
 import Demo2 from "./components/demo2.vue";
 import Demo3 from "./components/demo3.vue";
+import Demo4 from "./components/demo4.vue";
 export default {
   name: "App",
   components: {
     "v-demo1": Demo1,
     "v-demo2": Demo2,
     "v-demo3": Demo3,
+    "v-demo4": Demo4,
   },
 };
 </script>
