78fb01f86c8ae66aaa8aa4e54331519b2524f472
logicwang <1758555877@qq.com>
2022-02-23 21:53:45 +0800

    修改bug

--- demo14/src/components/demo1.vue
+++ demo14/src/components/demo1.vue
@@ -1,24 +1,19 @@
 <template>
 <div class="parent">
-  <p>我: {{sthGiveChild}}</p>
-  <Child @returnBack="turnBack" :give="sthGiveChild"></Child>
+    <p>父组件: {{sthGiveChild}}</p>
+    <Child v-model="sthGiveChild"></Child>
 </div>
 </template>
 <script>
-import Child from './child';
+import Child from "./child";
 export default {
   data() {
     return {
-      sthGiveChild: '小王'
+      sthGiveChild: "xxxxx",
     };
   },
   components: {
-    Child
+    Child,
   },
-  methods: {
-    turnBack(val) {
-      this.sthGiveChild = val;
-    }
-  }
-}
+};
 </script>
