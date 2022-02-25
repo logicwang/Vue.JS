45c2273c3db44674f2214e10fb0942c8e06cb909
logicwang <1758555877@qq.com>
2022-02-25 11:02:25 +0800

    删除某些文件

--- router/demo1/src/main.js
+++ router/demo1/src/main.js
@@ -1,17 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import user from './user.vue'
 
-//引用router.js
-import router from './router.js'
-
-import router from './router'
-Vue.config.productionTip = false
-    /* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    //一定要注入到vue的实例对象上
-    router,
-    components: { App },
-    template: '<App/>'
-})
+createApp(App).mount('#app')
