0288c3bc9df6e703f73622262b2cf016e0388b33
logicwang <1758555877@qq.com>
2022-02-27 08:48:19 +0800

    新建demo15

---
+++ demo15/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <hr />
+    <v-demo1></v-demo1>
+    <hr />
+  </div>
+</template>
+
+<script>
+import Demo1 from "./components/demo1.vue";
+export default {
+  name: "App",
+  components: {
+    "v-demo1": Demo1,
+  },
+};
+</script>
+
+<style>
+hr {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: 3px solid #ff000f;
+}
+div {
+  text-align: center;
+}
+</style>
