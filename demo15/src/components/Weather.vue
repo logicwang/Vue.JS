8122fa3053d0774c9080fbfa0821da61ec1c6b27
logicwang <1758555877@qq.com>
2022-02-27 18:53:30 +0800

    插槽

---
+++ demo15/src/components/Weather.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="header">
+      <h1>我是页头标题</h1>
+      <div>
+        <slot name="header"></slot>
+      </div>
+    </div>
+    <div class="footer">
+      <h1>我是页尾标题</h1>
+      <div>
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: "child1",
+};
+</script>
+ 
+<style scoped>
+.header {
+  color: white;
+}
+</style>
--- demo15/src/components/demo1.vue
+++ demo15/src/components/demo1.vue
@@ -1,6 +1,38 @@
 <template>
-  <div></div>
+  <div>
+    <div>slot内容分发</div>
+    <Weather>
+      <template slot="header">
+        <p>我是页头的具体内容</p>
+      </template>
+      <template slot="footer">
+        <p>我是页尾的具体内容</p>
+      </template>
+    </Weather>
+  </div>
 </template>
+ 
 <script>
-export default {};
+import Weather from "./Weather.vue";
+
+export default {
+  name: "father1",
+  components: {
+    Weather,
+  },
+};
 </script>
+<style>
+.footer {
+  background: pink;
+  color: black;
+}
+
+.header {
+  background: yellow;
+}
+p{
+  color: black;
+
+}
+</style>
