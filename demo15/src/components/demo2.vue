443c2facab538480f8ffe9f54530f25dfddecfaf
logicwang <1758555877@qq.com>
2022-02-27 19:06:35 +0800

    v插槽

---
+++ demo15/src/components/demo2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <!-- 插槽-插入 -->
+      <span slot="center" v-show="showIt"> {{message}}</span>
+    <div>
+      <span>左边</span>
+      <!-- slot：插槽   name="center"给插槽具名 -->
+      <slot name="center">
+        <!-- 插槽默认值 -->
+        <span>中间</span>
+      </slot>
+      <span>右边</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: "aaaaaaaaa",
+      showIt: 1,
+    };
+   },
+//     components:{
+//             cpn: {
+//                 template: '#cpn',
+//             }
+//         },
+};
+</script>
