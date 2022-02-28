363d98525aa77b09d8bb1c4079902a7a157d4f54
logicwang <1758555877@qq.com>
2022-02-28 09:07:26 +0800

    引入demo3

--- demo15/src/App.vue
+++ demo15/src/App.vue
@@ -5,17 +5,21 @@
     <hr />
     <v-demo2></v-demo2>
     <hr />
+    <v-demo3></v-demo3>
+    <hr />
   </div>
 </template>
 
 <script>
 import Demo1 from "./components/demo1.vue";
 import Demo2 from "./components/demo2.vue";
+import Demo3 from "./components/demo3.vue";
 export default {
   name: "App",
   components: {
     "v-demo1": Demo1,
     "v-demo2": Demo2,
+    "v-demo3": Demo3,
   },
 };
 </script>
