170f927058720fd0cec3babd226b5f88e12119dc
logicwang <1758555877@qq.com>
2022-03-04 13:30:15 +0800

    修改文件

--- vue-router/src/components/NotFound.vue
+++ vue-router/src/components/NotFound.vue
@@ -1,4 +1,4 @@
-<template>
+`<template>
   <div class="notfound">
       <div>
         <h1>404</h1>
--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -1,36 +1,6 @@
 import Vue from 'vue'
-// import Router from 'vue-router'
 import App from './components/App'
-// import About from './components/About'
-// import Good from './components/Good'
-// import Home from './components/Home'
-// import NotFound from './components/NotFound'
-// import User from './components/User'
 import { router } from './router/index'
-
-// const routes = [
-//     { path: '/', component: Home },
-//     { path: '/about', component: About },
-//     { path: '/good', component: Good },
-//     { path: '/user/:id', component: User },
-//     {
-//         path: '*',
-//         component: NotFound
-//     }
-// ]
-
-// // 3. 创建 router 实例，然后传 `routes` 配置
-// // 你还可以传别的配置参数, 不过先这么简单着吧。
-// const router = new Router({
-//     routes // (缩写) 相当于 routes: routes
-// });
-
-// Vue.use(Router);
-// Vue.config.productionTip = false;
-
-// new Vue({
-//     router
-// }).$mount('#app')
 
 new Vue({
     // el: '#app',
