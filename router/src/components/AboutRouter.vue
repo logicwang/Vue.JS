b4099420a312b9abd50f871a9041dfe6aa833bd1
logicwang <1758555877@qq.com>
2022-03-07 19:01:09 +0800

    优化页面

--- router/src/components/AboutRouter.vue
+++ router/src/components/AboutRouter.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
-        <h3>首页</h3>
         <router-link to="/home/game">
             <button>显示</button>
         </router-link>
-        <router-view></router-view>
     </div>
 </template>
