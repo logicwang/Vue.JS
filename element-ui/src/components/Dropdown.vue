740894c8111b3fa601d294108a0acbe9dc1f86c1
logicwang <1758555877@qq.com>
2022-03-12 12:35:18 +0800

    引入button组件

---
+++ element-ui/src/components/Dropdown.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <a-dropdown-button @click="handleButtonClick">
+      Dropdown
+      <a-menu slot="overlay" @click="handleMenuClick">
+        <a-menu-item key="1"> <a-icon type="user" />1st menu item </a-menu-item>
+        <a-menu-item key="2"> <a-icon type="user" />2nd menu item </a-menu-item>
+        <a-menu-item key="3"> <a-icon type="user" />3rd item </a-menu-item>
+      </a-menu>
+    </a-dropdown-button>
+    <a-dropdown-button>
+      Dropdown
+      <a-menu slot="overlay" @click="handleMenuClick">
+        <a-menu-item key="1"> <a-icon type="user" />1st menu item </a-menu-item>
+        <a-menu-item key="2"> <a-icon type="user" />2nd menu item </a-menu-item>
+        <a-menu-item key="3"> <a-icon type="user" />3rd item </a-menu-item>
+      </a-menu>
+      <a-icon slot="icon" type="user" />
+    </a-dropdown-button>
+    <a-dropdown-button disabled style="margin-left: 8px" @click="handleButtonClick">
+      Dropdown
+      <a-menu slot="overlay" @click="handleMenuClick">
+        <a-menu-item key="1"> <a-icon type="user" />1st menu item </a-menu-item>
+        <a-menu-item key="2"> <a-icon type="user" />2nd menu item </a-menu-item>
+        <a-menu-item key="3"> <a-icon type="user" />3rd item </a-menu-item>
+      </a-menu>
+    </a-dropdown-button>
+    <a-dropdown>
+      <a-menu slot="overlay" @click="handleMenuClick">
+        <a-menu-item key="1"> <a-icon type="user" />1st menu item </a-menu-item>
+        <a-menu-item key="2"> <a-icon type="user" />2nd menu item </a-menu-item>
+        <a-menu-item key="3"> <a-icon type="user" />3rd item </a-menu-item>
+      </a-menu> 
+      <a-button style="margin-left: 8px"> Button <a-icon type="down" /> </a-button>
+    </a-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'DropApp',
+  methods: {
+    handleButtonClick(e) {
+      console.log('click left button', e);
+    },
+    handleMenuClick(e) {
+      console.log('click', e);
+    },
+  },
+};
+</script>
