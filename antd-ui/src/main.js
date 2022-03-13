0b8678feac5e686d65b2d1a469601e73f48d9a9d
logicwang <1758555877@qq.com>
2022-03-13 12:48:42 +0800

    安装Antd插件

--- antd-ui/src/main.js
+++ antd-ui/src/main.js
@@ -1,14 +1,13 @@
-import Vue from 'vue';
-import Antd from 'ant-design-vue';
-import App from './App';
-import 'ant-design-vue/dist/antd.css';
-Vue.config.productionTip = false;
+  // src/main.js
+  import Vue from 'vue'
+  import { Button } from 'ant-design-vue';
+  import 'ant-design-vue/dist/antd.css'
+  import App from './App'
 
-Vue.use(Antd);
+  Vue.component(Button.name, Button)
 
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    components: { App },
-    template: '<App/>',
-});
+  Vue.config.productionTip = false
+
+  new Vue({
+      render: h => h(App)
+  }).$mount("#app");
