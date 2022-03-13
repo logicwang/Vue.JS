c0740db848513dd1d5723908ed1cd17407fde713
logicwang <1758555877@qq.com>
2022-03-13 16:40:26 +0800

    添加插件并引入新组建

--- antd-ui/src/App.vue
+++ antd-ui/src/App.vue
@@ -1,16 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div>
+      <a-button type="primary"> Primary </a-button>
+      <a-button>Default</a-button>
+      <a-button type="dashed"> Dashed </a-button>
+      <a-button type="danger"> Danger </a-button>
+      <a-config-provider :auto-insert-space-in-button="false">
+        <a-button type="primary"> 按钮 </a-button>
+      </a-config-provider>
+      <a-button type="primary"> 按钮 </a-button>
+      <a-button type="link"> Link </a-button>
+    </div>
+    <div id="components-button-demo-button-group">
+      <h4>Basic</h4>
+      <a-button-group>
+        <a-button>Cancel</a-button>
+        <a-button type="primary"> OK </a-button>
+      </a-button-group>
+      <a-button-group>
+        <a-button disabled> L </a-button>
+        <a-button disabled> M </a-button>
+        <a-button disabled> R </a-button>
+      </a-button-group>
+      <a-button-group>
+        <a-button type="primary"> L </a-button>
+        <a-button>M</a-button>
+        <a-button>M</a-button>
+        <a-button type="dashed"> R </a-button>
+      </a-button-group>
+
+      <h4>With Icon</h4>
+      <a-button-group>
+        <a-button type="primary"> <a-icon type="left" />Go back </a-button>
+        <a-button type="primary"> Go forward<a-icon type="right" /> </a-button>
+      </a-button-group>
+      <a-button-group>
+        <a-button type="primary" icon="cloud" />
+        <a-button type="primary" icon="cloud-download" />
+      </a-button-group>
+      <a-icon type="step-backward" />
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "aPP",
   components: {
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -20,6 +59,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#components-button-demo-button-group > h4 {
+  margin: 16px 0;
+  font-size: 14px;
+  line-height: 1;
+  font-weight: normal;
+}
+#components-button-demo-button-group > h4:first-child {
+  margin-top: 0;
+}
+#components-button-demo-button-group .ant-btn-group {
+  margin-right: 8px;
 }
 </style>
--- antd-ui/src/main.js
+++ antd-ui/src/main.js
@@ -1,13 +1,12 @@
-  // src/main.js
-  import Vue from 'vue'
-  import { Button } from 'ant-design-vue';
-  import 'ant-design-vue/dist/antd.css'
-  import App from './App'
+import Vue from 'vue';
+import 'ant-design-vue/dist/antd.css';
+import { Button } from 'ant-design-vue';
+import App from './App';
 
-  Vue.component(Button.name, Button)
+Vue.component(Button.name, Button);
+Vue.use(Button);
+Vue.config.productionTip = false;
 
-  Vue.config.productionTip = false
-
-  new Vue({
-      render: h => h(App)
-  }).$mount("#app");
+new Vue({
+    render: h => h(App),
+}).$mount('#app');
