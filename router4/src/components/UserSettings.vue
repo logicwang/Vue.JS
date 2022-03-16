90b602cb1ca675fa0ac53578db059e12f69eac4e
logicwang <1758555877@qq.com>
2022-03-16 09:47:32 +0800

    提交

--- router4/src/components/UserSettings.vue
+++ router4/src/components/UserSettings.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="us">
     <h2>User Settings</h2>
-    <UserSettingsNav />
     <router-view class="us__content" />
     <router-view name="helper" class="us__content us__content--helper" />
   </div>
 </template>
 <script>
 export default {
-    name:'UserSeting'
-}
+  name: "UserSeting",
+};
 </script>
