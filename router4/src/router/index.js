e935a35b7c429b8a1b4706d21b43ff82d28b3274
logicwang <1758555877@qq.com>
2022-03-16 08:59:07 +0800

    fix bug

--- router4/src/router/index.js
+++ router4/src/router/index.js
@@ -1,19 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import App from '@/components/App'
-import UserEmailsSubscriptions from '../components/UserEmailsSubscriptions'
-import UserProfilePreview from '../components/UserProfilePreview'
-import UserProfile from '../components/UserProfile'
-import UserSettings from '../components/UserSettings'
+import UserEmailsSubscriptions from '@/components/UserEmailsSubscriptions'
+import UserProfilePreview from '@/components/UserProfilePreview'
+import UserProfile from '@/components/UserProfile'
+import UserSettings from '@/components/UserSettings'
 
 
-export const routes = [
-    { path: '/', name: 'App', component: App },
-    { path: '/UserEmailsSubscriptions', name: 'UserEmailsSubscriptions', component: UserEmailsSubscriptions },
-    { path: '/UserProfilePreview', name: 'UserProfilePreview', component: UserProfilePreview },
-    { path: '/UserProfile', name: 'UserProfile', component: UserProfile },
-    { path: '/UserSettings', name: 'UserSettings', component: UserSettings },
-]
+export const routes = [{
+    path: '/settings',
+    // 你也可以在顶级路由就配置命名视图
+    component: UserSettings,
+    UserProfilePreview,
+    App,
+    children: [{
+        path: 'emails',
+        component: UserEmailsSubscriptions
+    }, {
+        path: 'profile',
+        components: {
+            default: UserProfile,
+            helper: UserProfilePreview
+        },
+    }]
+}]
 
 
 router.push('/settings/emails')
