8b7473f7b636c0375664fb56960a135bdbe3e0b9
logicwang <1758555877@qq.com>
2022-03-19 18:58:19 +0800

    fix:bug

--- router/router6/src/components/UserApp.vue
+++ router/router6/src/components/UserApp.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="user">
     <h4>用户信息</h4>
-    <div>参数: {{id}}</div>
+    <div>userId: {{$route.params.id}}</div>
   </div>
 </template>
-
-<script>
-export default {
-  props: ["id"]
-};
-</script>
