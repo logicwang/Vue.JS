f3ed732abcbbb2e4301071209277009179bf20ee
logicwang <1758555877@qq.com>
2022-04-04 07:48:11 +0800

    Submit all

--- RouterLearn/demo1/src/App.vue
+++ RouterLearn/demo1/src/App.vue
@@ -1,21 +1,20 @@
 <template>
-  <div id="app">
-    <v-header :seller="seller"></v-header>
-    <div class="tab">
-      <div class="tab-item border-1px">
-        <!-- 使用 router-link 组件来导航. -->
-        <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-        <router-link to="/goods">商品</router-link>
-      </div>
-      <div class="tab-item">
-        <router-link to="/ratings">评论</router-link>
-      </div>
-      <div class="tab-item">
-        <router-link to="/seller">商家</router-link>
-      </div>
+  <div>
+    <ul>
+      <router-link to="/home" tag="li" active-class="active">
+      <a href="javascript:;">首页</a>
+      </router-link>
+    </ul>
+  <div>
+    <transiition enter-active-class="animated zoomInLeft"
+    leave-active-class="animated zoomOutRight">
+    <router-view/>
+    </transiition>
     </div>
-    <!-- 路由出口 -->
-    <!-- 路由匹配到的组件将渲染在这里 -->
-    <router-view></router-view>
   </div>
 </template>
+<script>
+export default {
+  name:'AppLogin'
+}
+</script>
