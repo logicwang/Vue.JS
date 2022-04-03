9837b171b2318d38cd23e5d2b4c3656879277836
logicwang <1758555877@qq.com>
2022-04-03 18:56:54 +0800

    vue-element-admin

---
+++ vue-element-admin/src/views/error-page/403.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>您没有权限访问此页面。(回去照个镜子)</p>
+    <button class="button">返回首页啦白痴</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.button {
+  display: block;
+  float: left;
+  width: 110px;
+  height: 36px;
+  background: #1482f0;
+  border-radius: 100px;
+  text-align: center;
+  color: #ffffff;
+  opacity: 0;
+  font-size: 14px;
+  line-height: 36px;
+  cursor: pointer;
+  border:1px solid #1482f0;
+}
+</style>
