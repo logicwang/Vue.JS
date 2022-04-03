34fde27e129f1e8bd29aae804f794b251fa626f7
logicwang <1758555877@qq.com>
2022-04-03 19:23:27 +0800

    add new page

---
+++ vue-element-admin/src/views/weibo/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <button class="button-btn">
+          <a href="https://gitee.com/">gitee</a>
+      </button>
+  </div>
+</template>
+<style lang="scss">
+.button-btn{
+    margin:20px;
+    text-align: center;
+    background-color: aqua;
+    border: 1px solid aqua;
+    padding: 20px 20px 0px 0px ;
+}
+</style>
