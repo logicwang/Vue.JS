bc2d21fac897ca2196df3ab3b1918a1a635d6aa2
logicwang <1758555877@qq.com>
2022-04-05 18:22:52 +0800

    fix 404 notfound pages

--- RouterLearn/demo/src/components/NotFound.vue
+++ RouterLearn/demo/src/components/NotFound.vue
@@ -1,10 +1,8 @@
 `<template>
-  <div class="notfound">
-      <div>
-        <h1>404</h1>
-        <div>
-            页面不存在！！！！！
-        </div>
+  <div class="notfound bottom">
+      <div  >
+        <h1>404 Page Not Found</h1>
+        <p>The page you requested does not exist.</p>
       </div>
   </div>
 </template>
@@ -14,7 +12,10 @@
      min-height: 200px;
     display: flex;
     justify-content: center;
-        color: #afafaf;
+    color: #3d4246;
     align-items: center;
+}
+.bottom{
+  border: 1px solid #8994a3;
 }
 </style>
