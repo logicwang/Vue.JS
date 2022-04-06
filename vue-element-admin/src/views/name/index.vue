ed4ef607b297c4d6c23935f65219b319940cdc6f
logicwang <1758555877@qq.com>
2022-04-06 11:01:26 +0800

    fix bug

---
+++ vue-element-admin/src/views/name/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button onclick="openUrl();">Click here</button>
+
+    <a href="https://www.zhihu.com/" target="_blank"
+      ><button>Click here</button></a
+    >
+  </div>
+</template>
+<script type="text/javascript">
+openUrl = function (id) {
+  var url = "https://www.zhihu.com/";
+  window.open(url); //新窗口打开
+  window.location.href = url; //当前窗口打开
+};
+</script>
