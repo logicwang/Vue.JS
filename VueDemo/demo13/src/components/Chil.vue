7ca5f0d97cc8c14790c91fbc9142717a57630a0f
logicwang <1758555877@qq.com>
2022-04-12 00:04:17 +0800

    变更文件夹名称

--- VueDemo/demo13/src/components/Chil.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>{{ info }}</div>
-</template>
-<script>
-export default {
-  props: ["info"],
-};
-</script>
--- VueDemo/demo13/src/components/Chld.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>{{aaaaa}}</div>
-</template>
-<script>
-export default {
-  props:['aaaaa']
-}
-</script>
