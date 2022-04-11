b9400086ccff0f25b8263c10ba673db89bb9dbc9
logicwang <1758555877@qq.com>
2022-04-11 12:53:18 +0800

    添加插件然后新增页面

--- Elementui/demo/src/AnotherApp.vue
+++ Elementui/demo/src/AnotherApp.vue
@@ -1,8 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <LayOut />
+  </div>
 </template>
 <script>
+    import LayOut from './components/view/LayOut'
+
 export default {
-  name: "AnotherApp",
-};
+    comments:{
+        LayOut
+    }
+}
 </script>
