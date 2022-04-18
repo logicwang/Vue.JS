8e9767ee8095771f3b2371fd91269c1e845b6353
logicwang <1758555877@qq.com>
2022-04-18 22:12:52 +0800

    添加alert

--- Elementui/uidemo/src/App.vue
+++ Elementui/uidemo/src/App.vue
@@ -1,33 +1,38 @@
 <template>
   <div id="app">
-    <HelloWorld/>
-    <hr/>
-    <WhileApp/>
-    <hr/>
-    <CallApp/>
-    <hr/>
+    <HelloWorld />
+    <hr />
+    <WhileApp />
+    <hr />
+    <CallApp />
+    <hr />
+    <p>你看我出不出来</p>
+    <meta charset="UTF-8" />
+    <title></title>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import WhileApp from './components/WhileApp.vue'
-import CallApp from './components/CallApp.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import WhileApp from "./components/WhileApp.vue";
+import CallApp from "./components/CallApp.vue";
 
+alert("这是一个警告框");
 
 export default {
-  name: 'app',
+  name: "app",
+
   components: {
     HelloWorld,
     WhileApp,
-    CallApp
-  }
-}
+    CallApp,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
