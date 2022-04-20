5a5033878627e2fdf80a7e84d40188249b7d80cd
logicwang <1758555877@qq.com>
2022-04-20 12:18:15 +0800

    格式化代码

--- demo/demo1/src/components/ToodApp.vue
+++ demo/demo1/src/components/ToodApp.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app-4">
-  <ol>
-    <li v-for="todo in todos"  :key="todo">
-      {{ todo.text }}
-    </li>
-  </ol>
-</div>
+    <ol>
+      <li v-for="todo in todos" :key="todo">
+        {{ todo.text }}
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
 export default {
-data(){
-    return{
-         todos: [
-      { text: '学习 JavaScript' },
-      { text: '学习 Vue' },
-      { text: '整个牛项目' }
-    ]
-    }
-}
-}
+  data() {
+    return {
+      todos: [
+        { text: "学习 JavaScript" },
+        { text: "学习 Vue" },
+        { text: "整个牛项目" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
