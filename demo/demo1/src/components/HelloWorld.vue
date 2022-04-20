e096d82d4d548c089b8ea60a4d3522b3edbbdd03
logicwang <1758555877@qq.com>
2022-04-20 10:15:44 +0800

    第一次提交

---
+++ demo/demo1/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      message: "Hello Vue!",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
