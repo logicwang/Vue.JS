8feaab4779da8a372fe6a803700d3fb87852458c
logicwang <1758555877@qq.com>
2022-04-23 09:58:57 +0800

    v-model指令

---
+++ demo/demo1/src/components/ModelApp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app-6">
+    <p>{{ message }}</p>
+    <input v-model="message" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModelApp",
+  data() {
+    return {
+      message: "Hello Vue!",
+    };
+  },
+};
+</script>
+
+<style>
+input{
+    margin: 0;
+    margin-bottom: 5px;
+}
+</style>
