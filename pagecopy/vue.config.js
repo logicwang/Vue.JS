96444cf3d4768cb027b23187de536f8b494e824e
logicwang <1758555877@qq.com>
2022-04-30 09:52:48 +0800

    添加内容

--- pagecopy/vue.config.js
+++ pagecopy/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
+
+module.exports = {
+    pwa: {
+        iconPaths: {
+            favicon32: 'xiaomi.ico',
+            favicon16: 'xiaomi.ico',
+            appleTouchIcon: 'xiaomi.ico',
+            maskIcon: 'xiaomi.ico',
+            msTileImage: 'xiaomi.ico'
+        }
+    }
+}
