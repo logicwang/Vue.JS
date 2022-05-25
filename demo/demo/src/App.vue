2bdff8b54fdf69e72b41c5e188f0968d8ec11842
logicwang <1758555877@qq.com>
2022-05-25 17:18:19 +0800

    批量提交

---
+++ demo/demo/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ demo/demo/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>{{ message }}</div>
+    <div id="app-2">
+      <span v-bind:title="age">
+        鼠标悬停几秒钟查看此处动态绑定的提示信息！
+      </span>
+
+      <div id="app-3">
+        <p v-if="seen">现在你看到我了</p>
+      </div>
+
+      <div id="app-4">
+        <ol>
+          <li :key="todo" v-for="todo in todos">
+            {{ todo.text }}
+          </li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Vue!",
+
+      age: "页面加载于 " + new Date().toLocaleString(),
+
+      seen: true,
+
+      todos: [
+        { text: "学习 JavaScript" },
+        { text: "学习 Vue" },
+        { text: "整个牛项目" },
+      ],
+    };
+  },
+};
+</script>
