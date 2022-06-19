b78c8afa79b818d61861d01f36f4e7155031153a
logicwang <1758555877@qq.com>
2022-06-20 06:22:06 +0800

    修改搜索框内容

--- vue-element-admin/src/views/zhihu/index.vue
+++ vue-element-admin/src/views/zhihu/index.vue
@@ -10,10 +10,10 @@
         >
           <el-row>
             <el-col :span="6">
-              <el-form-item label="合同名称">
+              <el-form-item label="姓名">
                 <el-input
                   v-model.trim="searchForm.targetCompanyName"
-                  placeholder="请输入合同名称"
+                  placeholder="请输入列表数据"
                 ></el-input>
               </el-form-item>
             </el-col>
@@ -37,6 +37,31 @@
                 <el-button type="primary" @click="dialogFormVisible = true"
                   >内容</el-button
                 >
+                <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+                <el-table-column
+                  prop="address"
+                  label="地址"
+                  :formatter="formatter"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="tag"
+                  label="标签"
+                  width="100"
+                  :filters="[
+                    { text: '家', value: '家' },
+                    { text: '公司', value: '公司' },
+                  ]"
+                  :filter-method="filterTag"
+                  filter-placement="bottom-end"
+                >
+                  <template slot-scope="scope">
+                    <el-tag
+                      :type="scope.row.tag === '家' ? 'primary' : 'success'"
+                      disable-transitions
+                      >{{ scope.row.tag }}</el-tag>
+                  </template>
+                </el-table-column>
               </el-form-item>
             </el-col>
           </el-row>
@@ -50,8 +75,34 @@
 export default {
   data() {
     return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+          tag: "家",
+        },
+      ],
       searchForm: {},
     };
+  },
+  methods: {
+    // 计算属性
+    mounted() {
+      this.getLabelList();
+      this.getList();
+    },
+    // 重置列表
+    handleReset() {
+      this.searchForm = {};
+      this.getList();
+    },
+    formatter(row, column) {
+      return row.address;
+    },
+    filterTag(value, row) {
+      return row.tag === value;
+    },
   },
 };
 </script>
@@ -61,28 +112,31 @@ export default {
   margin-bottom: 0px;
 }
 .el-button {
-    display: inline-block;
-    line-height: 1;
-    white-space: nowrap;
-    cursor: pointer;
-    background: #FFFFFF;
-    border: 1px solid #DCDFE6;
-    border-color: #DCDFE6;
-    color: #606266;
-    -webkit-appearance: none;
-    text-align: center;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    outline: none;
-    margin: 0;
-    -webkit-transition: 0.1s;
-    transition: 0.1s;
-    font-weight: 400;
-    /* -moz-user-select: none;
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  background: #ffffff;
+  border: 1px solid #dcdfe6;
+  border-color: #dcdfe6;
+  color: #606266;
+  -webkit-appearance: none;
+  text-align: center;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  outline: none;
+  margin: 0;
+  -webkit-transition: 0.1s;
+  transition: 0.1s;
+  font-weight: 400;
+  /* -moz-user-select: none;
     -webkit-user-select: none;
     -ms-user-select: none; */
-    padding: 8px 15px;
-    font-size: 14px;
-    border-radius: 4px;
+  padding: 8px 15px;
+  font-size: 14px;
+  border-radius: 4px;
+}
+.el-form-item__label {
+  width: 50px;
 }
 </style>
